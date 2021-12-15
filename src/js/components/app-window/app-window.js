04a250d844bbae4fb0845051a8d220369e56a9ff
Anders Jonsson <aj224rj@student.lnu.se>
2021-12-15 21:37:17 +0100

    Add draggability to app-window

--- src/js/components/app-window/app-window.js
+++ src/js/components/app-window/app-window.js
@@ -3,7 +3,6 @@ const template = document.createElement('template')
 template.innerHTML = `
   <style>
     :host {
-      position: relative;
       display: flex;
       flex-direction: column;
       font-size: 10px;
@@ -13,7 +12,7 @@ template.innerHTML = `
     }
 
     #header {
-      height: 3em;
+      height: 5em;
       background-color: red;
     }
 
@@ -32,6 +31,16 @@ customElements.define('app-window',
    *
    */
   class extends HTMLElement {
+    #active = false
+    #currentX
+    #currentY
+    #initialX
+    #initialY
+    #xOffset = 0
+    #yOffset = 0
+
+    #header
+
     /**
      * Create instance of class and attach open shadow-dom.
      *
@@ -41,5 +50,60 @@ customElements.define('app-window',
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
+
+      this.#header = this.shadowRoot.querySelector('#header')
+
+      this.#header.addEventListener('mousedown', (event) => {
+        event.stopPropagation()
+        this.#dragStart(event)
+      })
+
+      this.#header.addEventListener('mouseup', (event) => {
+        event.stopPropagation()
+        this.#dragEnd()
+      })
+
+      this.#header.addEventListener('mousemove', (event) => {
+        event.stopPropagation()
+        this.#drag(event)
+      })
+    }
+
+    #dragStart (event) {
+      this.#initialX = event.clientX - this.#xOffset
+      this.#initialY = event.clientY - this.#yOffset
+
+      this.#active = true
+    }
+
+    #dragEnd () {
+      this.#initialX = this.#currentX
+      this.#initialY = this.#currentY
+
+      this.#active = false
+    }
+
+    #drag (event) {
+      if (this.#active) {
+        const rect = this.getBoundingClientRect()
+
+        if (rect.x < 0) {
+          this.#currentX = 0
+        } else if (rect.x + rect.width > window.innerWidth) {
+          this.#currentX = window.innerWidth - rect.width
+        } else {
+          this.#currentX = event.clientX - this.#initialX
+          this.#currentY = event.clientY - this.#initialY
+        }
+
+        this.#xOffset = this.#currentX
+        this.#yOffset = this.#currentY
+
+        this.#setTranslate(this.#currentX, this.#currentY)
+      }
+    }
+
+    #setTranslate(xPos, yPos) {
+      this.style.transform = `translate3d(${xPos}px, ${yPos}px, 0`
     }
   })

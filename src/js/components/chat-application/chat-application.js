0ba7feaf855f8426194a6971042ffd0f1a0acbd6
Anders Jonsson <aj224rj@student.lnu.se>
2022-01-05 07:25:36 +0100

    Add initial code to connect to websocket and in template

--- src/js/components/chat-application/chat-application.js
+++ src/js/components/chat-application/chat-application.js
@@ -1,3 +1,5 @@
+import * as constants from './lib/constants.js'
+
 const template = document.createElement('template')
 
 template.innerHTML = `
@@ -14,10 +16,36 @@ template.innerHTML = `
       align-items: center;
       width: 40em;
       min-height: 30em;
-      box-sizing: border-box;
+      border-radius: 3px;
+      background-color: var(--color-active-background);
     }
-    </style>
-  <div id="main"></div>
+
+    #chat-container {
+      position: relative;
+    }
+
+    #chat-banner {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      padding: .7em 0;
+      /*
+      TODO: continue here.
+      */
+    }
+
+    .hidden {
+      display: none !important;
+    }
+  </style>
+  <div id="main">
+    <div id="chat-container">
+      <div id="chat-banner">This is the banner</div>
+      <div id="chat"></div>
+    </div>
+    <div id="nickname"></div>
+  </div>
 `
 
 customElements.define('chat-application',
@@ -26,7 +54,41 @@ customElements.define('chat-application',
    *
    */
   class extends HTMLElement {
+    /**
+     * The websocket-connection object.
+     *
+     * @type {object}
+     */
     #websocket
+
+    /**
+     * The div-element holding the main-content.
+     *
+     * @type {HTMLElement}
+     */
+    #mainElement
+
+    /**
+     * The div-element holding the chat-content.
+     *
+     * @type {HTMLElement}
+     */
+    #chatContainerElement
+
+    /**
+     * The div-element holding the chat-conversation.
+     *
+     * @type {HTMLElement}
+     */
+    #chatElement
+
+    /**
+     * The div element holding nickname-form.
+     *
+     * @type {HTMLElement}
+     */
+    #nicknameElement
+
     /**
      * Create instance of class and attach open shadow-dom.
      *
@@ -36,6 +98,11 @@ customElements.define('chat-application',
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
+
+      this.#mainElement = this.shadowRoot.querySelector('#main')
+      this.#chatContainerElement = this.shadowRoot.querySelector('#chat-container')
+      this.#chatElement = this.shadowRoot.querySelector('#chat') // NOTE: Needed???
+      this.#nicknameElement = this.shadowRoot.querySelector('#nickname')
     }
 
     /**
@@ -43,22 +110,63 @@ customElements.define('chat-application',
      *
      */
     connectedCallback () {
-      this.#websocket = new WebSocket('wss://courselab.lnu.se/message-app/socket', 'chatApp')
-      console.log(this.#websocket)
-      this.#websocket.onopen = (event) => {
-        console.log(event)
-        this.#websocket.send("Here's some text that the server is urgently awaiting!");
+      if (window.localStorage.getItem('chatapp-nickname')) {
+        this.#initChat()
       }
+    }
 
+    /**
+     * Close websocket connection.
+     *
+     */
+    disconnectedCallback () {
+      this.#websocket.close()
+      // TODO: remove eventlisteners.
+    }
+
+    #initChat () {
+      try {
+        this.#hideAllInMain()
+        this.#chatContainerElement.classList.remove('hidden')
+        this.#connect()
+      } catch (error) {
+        console.log(error)
+        // TODO: handle scenario when unnable to connect.
+      }
+    }
+
+    /**
+     * Connect to the websocket.
+     *
+     */
+    #connect () {
+      // test-data
       const data = {
         type: 'message',
         data: 'Hello wss',
         username: 'AndersJson',
-        key: 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
+        key: constants.API_KEY
       }
+
+      this.#websocket = new WebSocket('wss://courselab.lnu.se/message-app/socket', 'chatApp')
+
+      this.#websocket.addEventListener('open', event => {
+        console.log(event)
+        this.#websocket.send(JSON.stringify(data))
+      })
+
+      this.#websocket.addEventListener('message', event => {
+        console.log(event.data)
+      })
     }
 
-    async #connect () {
-      //
+    /**
+     * Hide all elements in content-div.
+     *
+     */
+    #hideAllInMain () {
+      for (const element of this.#mainElement.children) {
+        element.classList.add('hidden')
+      }
     }
   })
---
+++ src/js/components/chat-application/lib/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
